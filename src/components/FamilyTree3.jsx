d0bdb9a8d749e11bbdaf517c08f4055e1c3af5e9
Felix <dexdaprotocol@gmail.com>
2023-08-09 12:35:50 +0200

    updates

--- src/components/FamilyTree3.jsx
+++ src/components/FamilyTree3.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import FamilyTree from "../components/FamilyTree";
 export default function FamilyTree3() {
   return (
     <div className="bg-gray-900 w-full">
@@ -45,39 +46,16 @@ export default function FamilyTree3() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-7xl px-6 pb-12 pt-10 sm:pb-24 lg:flex lg:px-8 lg:pt-40">
-          <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-            <img
-              className="h-11"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
 
-            <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Blockcestry
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Build out your familytree, share files and reconnect with your
-              relatives.
-            </p>
-            <div className="mt-10 flex items-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-              >
-                Get started
-              </a>
+        <div className="mx-auto max-w-7xl px-6 flex flex-col justify-center items-center pb-12 pt-10 sm:pb-24 lg:flex lg:px-8 lg:pt-24">
+          <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+            <div className="text-2xl lg:mb-16 font-bold tracking-tight text-white sm:text-4xl">
+              FAMILYTREE
             </div>
           </div>
           <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-            <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-              <img
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                alt="App screenshot"
-                width={2432}
-                height={1442}
-                className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-              />
+            <div className="max-w-3xl mb-48 flex-none sm:max-w-5xl lg:max-w-none">
+              <FamilyTree />
             </div>
           </div>
         </div>
