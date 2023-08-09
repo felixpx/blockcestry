1d2fb45d1de0de3751fd62819b5269c48dfea1f8
Felix <dexdaprotocol@gmail.com>
2023-08-09 12:24:50 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,13 +58,13 @@ export default function Header({ title }) {
                     Familytree
                   </a>
                   <a
-                    href="#"
+                    href="profile"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
                     Profile
                   </a>
                   <a
-                    href="#"
+                    href="timecapsule"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
                     Time Capsule
